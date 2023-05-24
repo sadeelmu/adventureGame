fb73b246716ac559134c3fd25d2980bfa14114b3
Sadel Muwahed <sadeel.mu15@gmail.com>
2023-05-24 09:28:21 +0300

    First push for mobile application development final project

---
+++ AdventureGame/app/src/main/java/com/example/adventuregame/TitleScreen.kt
@@ -0,0 +1,11 @@
+package com.example.adventuregame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TitleScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_title_screen)
+    }
+}
